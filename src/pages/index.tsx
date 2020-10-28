5c8eb03dcd362d1403cbd845a7b020be72896c15
huc <ht@Live.se>
2020-10-28 17:33:38 +0800

    update dependence

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,66 @@
 import React from "react"
+import { Stack, Text, Link, FontWeights } from "@fluentui/react"
+import "./index.css"
 
-import { Link } from "gatsby"
-export default () => (
-  <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <Link to="/about">about</Link>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-    
-  </div>
-)
+const boldStyle = { root: { fontWeight: FontWeights.semibold } }
+
+const App: React.FunctionComponent = () => {
+  return (
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: "960px",
+          margin: "0 auto",
+          textAlign: "center",
+          color: "#605e5c",
+        },
+      }}
+      className="App"
+      gap={15}
+    >
+      <img
+        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
+        alt="logo"
+      />
+      <Text variant="xxLarge" styles={boldStyle}>
+        Welcome to Your UI Fabric App
+      </Text>
+      <Text variant="large">
+        For a guide on how to customize this project, check out the UI Fabric
+        documentation.
+      </Text>
+      <Text variant="large" styles={boldStyle}>
+        Essential Links
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
+        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
+          Stack Overflow
+        </Link>
+        <Link href="https://github.com/officeDev/office-ui-fabric-react/">
+          Github
+        </Link>
+        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
+      </Stack>
+      <Text variant="large" styles={boldStyle}>
+        Design System
+      </Text>
+      <Stack horizontal gap={15} horizontalAlign="center">
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">
+          Icons
+        </Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">
+          Typography
+        </Link>
+        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">
+          Theme
+        </Link>
+      </Stack>
+    </Stack>
+  )
+}
+
+export default App
